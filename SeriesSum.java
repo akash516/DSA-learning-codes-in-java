92f670355cfa5b04ad431cd7bb351b433ea9d8fa
Akash Das <akash03062006@gmail.com>
2026-01-02 13:47:29 +0530

    added even odd sum method

--- SeriesSum.java
+++ SeriesSum.java
@@ -4,12 +4,22 @@ public class SeriesSum{
        if(n==0) return 0;
        return seriessum1(n-1)+n; 
     }
+    static int seriessum2(int n){
+        if(n==0) return 0;
+        if(n%2==0){//even
+            return seriessum2(n-1)-n;
+        }
+        else{
+            return seriessum2(n-1)+n;
+        }
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter n: ");
         int n=sc.nextInt();
         System.out.printf("Sum of series= ");
         System.out.println(seriessum1(n));
+        System.out.println("Sum of even odd series="+ seriessum2(n));
         sc.close();
     }
 }
