373c371212ebd3c7c2535c55640e80ad05752ccd
Akash Das <akash03062006@gmail.com>
2026-01-05 13:58:57 +0530

    Added Sum of Array, Max and Min using Recursion

--- ArrayRecursion101.java
+++ ArrayRecursion101.java
@@ -7,6 +7,26 @@ public class ArrayRecursion101{
        System.out.print(arr[idx]+", ");
        printarr(arr,idx+1);
     }
+    static int Max_Recursion(int [] arr, int idx){
+        if(idx==arr.length-1){
+            return arr[idx];
+        }
+        int smallans=Max_Recursion(arr, idx+1);
+      return Math.max(smallans,arr[idx]);
+   }
+   static int Min_Recursion(int [] arr, int idx){
+        if(idx==arr.length-1){
+            return arr[idx];
+        }
+        int smallans=Min_Recursion(arr, idx+1);
+      return Math.min(smallans,arr[idx]);
+   }
+   static int Sum_Recursion(int [] arr, int idx) {
+       if(idx==arr.length-1){
+        return arr[idx];
+       }
+       return arr[idx]+Sum_Recursion(arr, idx+1);
+   }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the length of array:");
@@ -17,6 +37,9 @@ public class ArrayRecursion101{
             arr[i]=sc.nextInt();
         }
     printarr(arr,0);
+    System.out.println("Maximum value:"+Max_Recursion(arr, 0));
+    System.out.println("Minimum value:"+Min_Recursion(arr, 0));
+    System.out.println("Sum of all elements: "+Sum_Recursion(arr, 0));
     sc.close();
     }
 }
